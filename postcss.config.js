2f59c299a47a663d6c5b2668019f3b22ed133c46
kaave <abe@framelunch.jp>
2018-05-19 13:49:11 +0900

    remove unuse postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -3,16 +3,14 @@ const plugins = {
   'postcss-custom-properties': {},
   'postcss-custom-media': {},
   'postcss-nested': {},
-  'postcss-mixins': {},
   'postcss-color-hex-alpha': {},
-  'postcss-simple-vars': {},
   'postcss-fixes': {},
   'postcss-url': {},
-  'autoprefixer': {},
+  autoprefixer: {},
 };
 
 if (process.env.NODE_ENV === 'production') {
   plugins.cssnano = {};
-};
+}
 
 module.exports = { plugins };
