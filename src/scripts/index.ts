cc6bf481a76ad12d53ee39b2b830eb9abf6e867f
kaave <abe@framelunch.jp>
2018-08-02 23:36:21 +0900

    index: add simple code

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -2,6 +2,7 @@ import 'babel-polyfill'; // アプリ内で1度だけ読み込む エントリ�
 import * as PIXI from 'pixi.js';
 
 import '../styles/index.css';
+<<<<<<< HEAD
 PIXI.utils.skipHello();
 
 function loadImages() {
@@ -26,4 +27,32 @@ Promise.all([loadImages(), loadDOM()]).then(() => {
   stage.addChild(sprite);
 
   document.body.appendChild(renderer.view);
+=======
+
+PIXI.utils.skipHello();
+
+function getResource(): Promise<PIXI.loaders.Resource> {
+  return new Promise(resolve => {
+    const loader = PIXI.loader.add({ name: 'unsplash', url: 'alex-iby-628881-unsplash.jpg' });
+    loader.load(() => resolve(loader.resources.unsplash));
+  });
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+  const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+  renderer.autoResize = true;
+  document.body.appendChild(renderer.view);
+
+  const stage = new PIXI.Container();
+  const resource = await getResource();
+  const { texture } = resource;
+  const sprite = new PIXI.Sprite(texture);
+  sprite.position.set(0, 0);
+  sprite.width = renderer.width;
+  sprite.height = renderer.height;
+  sprite.texture.frame = new PIXI.Rectangle(0, 0, texture.width, texture.height);
+
+  stage.addChild(sprite);
+  renderer.render(stage);
+>>>>>>> index: add simple code
 });
