8af28d9b3390fb91e5985f9260aa4ebb90b25ce7
kaave <abe@framelunch.jp>
2018-08-10 18:36:31 +0900

    index: cleanup

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,133 +1,30 @@
 import 'babel-polyfill'; // アプリ内で1度だけ読み込む エントリーポイントのてっぺん推奨
 import * as PIXI from 'pixi.js';
-import Easing from 'bezier-easing';
 
 import '../styles/index.css';
-import Invert from './filters/Twist';
-
 PIXI.utils.skipHello();
-const easeInOutQuart = Easing(0.86, 0, 0.07, 1);
 
-const sliceLength = 200;
-class Main {
-  renderer: PIXI.WebGLRenderer | PIXI.CanvasRenderer;
-  stage: PIXI.Container;
-  filters?: {
-    invert: Invert;
-  };
-  sprites: PIXI.Sprite[];
-  animatePixels: number[];
-  animationID: number | null;
-  animationReverse: boolean;
-
-  constructor() {
-    this.animate = this.animate.bind(this);
-    this.onScroll = this.onScroll.bind(this);
-    this.renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
-    this.stage = new PIXI.Container();
-    this.sprites = [];
-    this.animatePixels = [];
-    this.animationID = null;
-    this.animationReverse = false;
-
-    const spriteWidth = this.renderer.width / sliceLength;
-    PIXI.loader.add({ name: 'unsplash', url: 'alex-iby-628881-unsplash.jpg' }).load(() => {
-      const container = new PIXI.Container();
-      const resource = PIXI.loader.resources.unsplash;
-      console.log(resource.texture.width, resource.texture.height);
-      [...Array(sliceLength).keys()].forEach(i => {
-        const texture = resource.texture.clone();
-        const sprite = new PIXI.Sprite(resource.texture.clone());
-        sprite.position.set(spriteWidth * i, 0);
-        sprite.width = this.renderer.width;
-        sprite.height = this.renderer.height;
-        sprite.texture.frame = new PIXI.Rectangle(
-          texture.width / sliceLength * i,
-          0,
-          texture.width / sliceLength,
-          texture.height,
-        );
-
-        container.addChild(sprite);
-        this.sprites.push(sprite);
-      });
-      this.filters = {
-        invert: new Invert(),
-      };
-
-      // container.filters = Object.values(this.filters);
-      this.stage.addChild(container);
-      this.stage.filters = Object.values(this.filters);
-      this.updateFilter();
-      this.updateRenderer();
-      window.addEventListener('scroll', this.onScroll);
-    });
-
-    this.renderer.autoResize = true;
-
-    document.body.appendChild(this.renderer.view);
-
-    setInterval(() => {
-      if (this.filters) {
-        // const setValue = Math.random();
-        this.filters.invert.timer += 1;
-        this.renderer.render(this.stage);
-      }
-    }, 1000 / 24);
-
-    // this.startAnimation(this.animationReverse);
-  }
-
-  onScroll() {
-    this.updateFilter();
-  }
-
-  updateFilter() {
-    // const heightRate = window.pageYOffset / document.body.getBoundingClientRect().height;
-    // if (this.filters) {
-    //   // this.filters.blur.blur = heightRate * 10;
-    // }
-    this.updateRenderer();
-  }
-
-  updateRenderer() {
-    this.renderer.render(this.stage);
-  }
-
-  startAnimation(isReverse: boolean) {
-    const fps = 60;
-    const animationSec = 1.5;
-    const length = fps * animationSec;
-
-    this.animatePixels = [...Array(length).keys()].map(
-      index => easeInOutQuart(index / (length - 1)) * window.innerHeight,
-    );
-    if (isReverse) {
-      this.animatePixels = this.animatePixels.reverse();
-    }
-    this.animationID = requestAnimationFrame(this.animate);
-  }
-
-  animate(_time: number) {
-    const movePx = this.animatePixels.shift();
-    if (movePx == null) {
-      if (this.animationID) {
-        cancelAnimationFrame(this.animationID);
-        this.animationID = null;
-        this.animationReverse = !this.animationReverse;
-        setTimeout(() => this.startAnimation(this.animationReverse), 100);
-      }
-
-      return;
-    }
-
-    this.sprites.forEach((sprite, index) => (sprite.y = index % 2 === 0 ? movePx * 1 : movePx * -1));
-    this.updateRenderer();
-    requestAnimationFrame(this.animate);
-  }
+function loadImages() {
+  return new Promise(resolve =>
+    PIXI.loader.add({ name: 'unsplash', url: 'alex-iby-628881-unsplash.jpg' }).load(resolve),
+  );
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-  const main = new Main();
-  console.log(main);
+function loadDOM() {
+  return new Promise(resolve => window.addEventListener('DOMContentLoaded', resolve));
+}
+
+Promise.all([loadImages(), loadDOM()]).then(() => {
+  const renderer = PIXI.autoDetectRenderer(window.innerWidth, window.innerHeight);
+  const stage = new PIXI.Container();
+  renderer.autoResize = true;
+  const { texture } = PIXI.loader.resources.unsplash;
+
+  const sprite = new PIXI.Sprite(texture);
+  sprite.position.set(0, 0);
+  sprite.texture.frame = new PIXI.Rectangle(0, 0, texture.width, texture.height);
+
+  stage.addChild(sprite);
+  renderer.render(stage);
+  document.body.appendChild(renderer.view);
 });
